5660df9cbd0d5ef558abe74dacf30cd8884b33c1
Yogi Ferdiansyah <yogif346@gmail.com>
2025-05-01 04:35:44 +0700

    yaya

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
 
 @Controller('payments')
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
+
+  @Post('status')
+  getPaymentStatus(@Body() statusData: any) {
+    console.log(statusData);
+  }
 }
