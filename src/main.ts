8f1fa9f25ef437f58d36dcb927e95b6e42f720cd
Yogi Ferdiansyah <yogif346@gmail.com>
2025-06-20 05:45:34 +0700

    adding swagger docs (not finished yet)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,15 @@ async function bootstrap() {
 
   const host = process.env.HOST || '0.0.0.0';
   const port = process.env.PORT || 5000; //jalan di port 5000
+
+  const config = new DocumentBuilder()
+    .setTitle('Subply API example')
+    .setDescription('This is subply api')
+    .setVersion('1.0')
+    .addTag('subply')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(port, host);
 }
